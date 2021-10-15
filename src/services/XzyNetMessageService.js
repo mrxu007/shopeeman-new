483177f56d414b807004e26604838aab44bcc457
dengyingxiang <1812532334@qq.com>
2021-10-15 10:14:24 +0800

    xzyService

--- src/services/XzyNetMessageService.js
+++ src/services/XzyNetMessageService.js
@@ -1,15 +1,19 @@
 /*
  * @Author: your name
  * @Date: 2021-10-13 16:56:39
- * @LastEditTime: 2021-10-14 20:32:06
+ * @LastEditTime: 2021-10-15 10:13:57
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \shopeeman-new\src\services\XzyNetMessageService.js
  */
 export default class XzyNetMessageService {
     baseUrl='http://api.xzy.com'
+    baseUrlA = ''
     constructor() {
       this.nativeService = window['XzyNetMessageBridgeService']
+    }
+    getA(apiName,params) {
+      return this.nativeService.get(this.baseUrlA+apiName, JSON.stringify(params))
     }
     get(apiName,params) {
       return this.nativeService.get(this.baseUrl+apiName, JSON.stringify(params))
