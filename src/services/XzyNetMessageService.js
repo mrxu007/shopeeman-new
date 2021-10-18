2d67ca894b2171d2ab367a4dddd5f88e4f8475b8
徐建周 <xujianzhou@91cyt.com>
2021-10-18 10:31:52 +0800

    1、 最新 10-18

--- src/services/XzyNetMessageService.js
+++ src/services/XzyNetMessageService.js
@@ -7,23 +7,18 @@
  * @FilePath: \shopeeman-new\src\services\XzyNetMessageService.js
  */
 export default class XzyNetMessageService {
-<<<<<<< HEAD
-    baseUrl='http://api.xzy.com/api/route'
-    baseUrlA = ''
-=======
-    baseUrl='http://api.xzy.com'
-    baseUrlA = 'http://user.xzygyl.com'
->>>>>>> d61e27e3c7b2e00d2bac764d267e013cd99ffbb9
-    constructor() {
-      this.nativeService = window['XzyNetMessageBridgeService']
-    }
-    getA(apiName, params) {
-      return this.nativeService.get(this.baseUrlA + apiName, JSON.stringify(params))
-    }
-    get(apiName, params) {
-      return this.nativeService.get(this.baseUrl + apiName, JSON.stringify(params))
-    }
-    post(apiName, data, options = { 'Content-Type': 'application/json' }) {
-      return this.nativeService.post(`${this.baseUrl}`, apiName, JSON.stringify({ headers: options }), data ? JSON.stringify(data) : null)
-    }
+  baseUrl = 'http://api.xzy.com'
+  baseUrlA = 'http://user.xzygyl.com'
+  constructor() {
+    this.nativeService = window['XzyNetMessageBridgeService']
+  }
+  getA(apiName, params) {
+    return this.nativeService.get(this.baseUrlA + apiName, JSON.stringify(params))
+  }
+  get(apiName, params) {
+    return this.nativeService.get(this.baseUrl + apiName, JSON.stringify(params))
+  }
+  post(apiName, data, options = { 'Content-Type': 'application/json' }) {
+    return this.nativeService.post(`${this.baseUrl}`, apiName, JSON.stringify({ headers: options }), data ? JSON.stringify(data) : null)
+  }
 }
