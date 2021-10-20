f6c296b1a2464d205489fbeb37cf9bad64bd1cce
徐建周 <xujianzhou@91cyt.com>
2021-10-20 10:01:01 +0800

    feature: 合并

--- src/module-api/mall-manager-api/mall-list-api.js
+++ src/module-api/mall-manager-api/mall-list-api.js
@@ -13,7 +13,6 @@ export async function getMallListAPI(params) {
   }
 }
 
-<<<<<<< HEAD
 // 更新店铺水印
 export async function updateWatermarkAPI(params) {
   try {
@@ -37,17 +36,15 @@ export async function updateUserPasswordAPI(params) {
       return { code: 200, data: res.data.data }
     }
     return { code: res.data.code, data: `${res.data.code} ${res.data.message}` }
-=======
-// 联动站点分组获取店铺列表
-export async function ddMallGoodsGetMallList(params) {
-  try {
-    const res = await api.ddMallGoodsGetMallList({ params })
-    if (res.data.code === 200) {
-      return { code: 200, data: res.data.data }
+    // 联动站点分组获取店铺列表
+    export async function ddMallGoodsGetMallList(params) {
+      try {
+        const res = await api.ddMallGoodsGetMallList({ params })
+        if (res.data.code === 200) {
+          return { code: 200, data: res.data.data }
+        }
+        return { code: -2, data: '获取店铺列表失败' }
+      } catch (error) {
+        return { code: -2, data: `getMallList-catch: ${error}` }
+      }
     }
-    return { code: -2, data: '获取店铺列表失败' }
->>>>>>> 475151fcb50787224a782d2f99389b048200344a
-  } catch (error) {
-    return { code: -2, data: `getMallList-catch: ${error}` }
-  }
-}
