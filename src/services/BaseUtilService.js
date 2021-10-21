4807a74d287c9e730c5141b4c9efae9712460602
徐建周 <xujianzhou@91cyt.com>
2021-10-21 09:45:37 +0800

    '1、添加基础服务'

---
+++ src/services/BaseUtilService.js
@@ -0,0 +1,52 @@
+export default class BaseUtilService {
+  constructor() {
+    this.nativeService = window['BaseUtilBridgeService']
+  }
+  stringGetEncodingGbk(writeString) {
+    return this.nativeService.stringGetEncodingGbk(writeString.toString())
+  }
+  getTbAddressModel(addressName) {
+    return this.nativeService.getTbAddressModel(addressName)
+  }
+  cookieRemove(cookie, removeName) {
+    return this.nativeService.cookieRemove(cookie.toString(), removeName.toString())
+  }
+  getMallIp(sysMallId) { // 获取店铺下绑定的ip
+    return this.nativeService.getMallIp(sysMallId.toString())
+  }
+  buildGoodCode(platformId, oriGoodsId) { // 商品编码加密
+    return this.nativeService.buildGoodCode(platformId, oriGoodsId, '')
+  }
+  getLogisticByOrderIdAsync(shotOrderAccount, orderSn) { // 淘宝物流获取
+    return this.nativeService.getLogisticByOrderIdAsync(shotOrderAccount, orderSn)
+  }
+  getTbLogisticByOrderIdAsync(shotOrderAccount, orderSn) { // 新淘宝物流获取
+    return this.nativeService.getTbLogisticByOrderIdAsync(shotOrderAccount, orderSn)
+  }
+  getJxLogisticByOrderIdAsync(shotOrderAccount, orderSn) { // 京东物流获取
+    return this.nativeService.getJxLogisticByOrderIdAsync(shotOrderAccount, orderSn)
+  }
+  getAliBaBaLogisticByOrderIdAsync(shotOrderAccount, orderSn) { // 1688物流获取
+    return this.nativeService.getAliBaBaLogisticByOrderIdAsync(shotOrderAccount, orderSn)
+  }
+  aliBaBaRefreshPersonCenterAsync(shotOrderAccount) { // 1688维活
+    return this.nativeService.aliBaBaRefreshPersonCenterAsync(shotOrderAccount)
+  }
+  /**
+   * 转换cookie注入到头部
+   * @param {*} str
+   * @returns
+   */
+  changeCookies(str) {
+    return this.nativeService.stringToCookies(str)
+  }
+  getPurchaseUa() {
+    return this.nativeService.getPurchaseUa()
+  }
+  openUrl(url) {
+    return this.nativeService.openUrl(url)
+  }
+  checkVersion(version) {
+    return this.nativeService.checkVersion(version)
+  }
+}
---
+++ src/services/collect-service.js
@@ -0,0 +1,45 @@
+export default class CollectService{
+    nativeService=window["CollectBridgeService"];
+
+
+    /**
+     *拼多多整店采集
+     * @param {PddMallInformation:{
+    *  ShopId:string,
+    *  TokenParams:string, token_params={"token": "4NMSL7R342JS54P5Y2Y6MBFRF2E46G2LLYMZWNAXZTE3CX256YFQ1101851", "ua": "Mozilla/5.0 (Linux; Android 7.1.2; m3 note Build/NJH47F; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/69.0.3497.109 Mobile Safari/537.36 pddmt_android_version/2.5.10 pddmt_android_build/2051001 pddmt_android_channel/qihu360", "pdduid": "17752623061", "api_uid": "CiHWXF9pwc9gEQBZPLf+Ag==", "ip": "27.42.139.193:44288"}
+    * }}  mallInformationModel
+    */
+    searchPddMallGoodsAsync(mallInformationModel){
+        return  this.nativeService.callFunction("SearchPddMallGoodsAsync",JSON.stringify(mallInformationModel)); 
+    }
+    /**
+     * 淘宝整店采集
+     * @param {TbMallInformation:{
+        *  ShopId:string,
+        *  TokenParams:string, token_params={"token": "4NMSL7R342JS54P5Y2Y6MBFRF2E46G2LLYMZWNAXZTE3CX256YFQ1101851", "ua": "Mozilla/5.0 (Linux; Android 7.1.2; m3 note Build/NJH47F; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/69.0.3497.109 Mobile Safari/537.36 pddmt_android_version/2.5.10 pddmt_android_build/2051001 pddmt_android_channel/qihu360", "pdduid": "17752623061", "api_uid": "CiHWXF9pwc9gEQBZPLf+Ag==", "ip": "27.42.139.193:44288"}
+        * }}  mallInformationModel
+     */
+    searchtbMallGoodsAsync(mallInformationModel){
+        return  this.nativeService.callFunction("SearchTbMallGoodsAsync",JSON.stringify(mallInformationModel)); 
+    }
+    /**
+     * 链接分析
+     * @param {*} searchLink url链接
+     */
+    getMallByLink(searchLink){
+        return this.nativeService.getMallByLink(searchLink)
+    }
+
+    /**
+     *京喜整店采集
+     * @param {JxMallInformation:{
+    *  ShopId:string,1000350045
+    *  Url:
+    *  SortType:string,//sort_totalsales15_desc  销量排序，sort_totalsales15_desc  价格升序，sort_dredisprice_asc  价格降序,
+    *  page:number,
+    * }}  mallInformationModel
+    */
+    searchJxMallGoodsAsync(mallInformationModel){
+        return  this.nativeService.callFunction("SearchJxMallGoodsAsync",JSON.stringify(mallInformationModel)); 
+    }
+}
