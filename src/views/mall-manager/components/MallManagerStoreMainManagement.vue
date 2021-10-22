6c4d8a16512b161803610e73c4782fdbf1452d16
陈紫微 <chenziwei@91cyt.com>
2021-10-22 11:59:13 +0800

    店铺主体管理接口对接

--- src/views/mall-manager/components/MallManagerStoreMainManagement.vue
+++ src/views/mall-manager/components/MallManagerStoreMainManagement.vue
@@ -63,8 +63,8 @@
       </div>
 
       <div class="condition_item">
-        <span>主题名称：</span>
-        <el-input v-model="query.orderID" placeholder="订单编号" size="mini" style="width:180px" />
+        <span>主体名称：</span>
+        <el-input v-model="query.orderID" placeholder="主体名称" size="mini" style="width:180px" />
       </div>
 
       <div class="condition_item">
@@ -72,13 +72,13 @@
       </div>
     </div>
     <div class="base_option_button" style="margin: 10px;">
-      <el-button size="mini" type="primary">新增公司主体</el-button>
+      <el-button size="mini" type="primary" @click="(Typeis='ipMaster',dialogvisible=true)">新增公司主体</el-button>
       <!-- <el-button size="mini" type="primary">解绑主体IP</el-button> -->
       <!-- <el-button size="mini" type="primary">绑定主体IP</el-button> -->
       <el-button size="mini" type="primary">清除IP缓存</el-button>
-      <el-button size="mini" type="primary">新增自有IP公司主体</el-button>
-      <el-button size="mini" type="primary">续费一个月</el-button>
-      <el-button size="mini" type="primary">续费三个月</el-button>
+      <el-button size="mini" type="primary" @click="(Typeis='ipPerson',dialogvisible=true)">新增自有IP公司主体</el-button>
+      <el-button size="mini" type="primary" @click="timeToMonth(1)">续费一个月</el-button>
+      <el-button size="mini" type="primary" @click="timeToMonth(2)">续费三个月</el-button>
     </div>
 
     <div style="margin:10px">
@@ -138,57 +138,122 @@
         </div>
       </div>
     </div>
-    <!-- dialog -->
-    <div>
+    <!-- dialog 新增公司主体-->
+    <div class="dialog_addip">
       <el-dialog
-        title="新增主体"
-        :visible.sync="dialog_addmaster"
-        width="30%"
-        :before-close="handleClose"
+        :title="Typeis==='ipMaster'?'新增公司主体':'新增自有IP公司主体'"
+        :visible.sync="dialogvisible"
+        width="1200px"
+        height="700px"
+        top="2vh"
       >
-        <div class="dialog_addip">
-          <div class="left">
+        <div class="left">
+          <!--新增公司主体  -->
+          <div v-if="Typeis==='ipMaster'">
             <div class="left_item">
               ip区域：
-              <el-select v-model="region_ip">
+              <el-select v-model="region_ip" size="mini">
                 <el-option v-for="(item,index) in region_ipList" :key="'region'+index" :label="item.value" :value="item.id" />
               </el-select>
             </div>
             <div class="left_item">
               ip周期：
-              <el-select v-model="time_ip">
+              <el-select v-model="time_ip" size="mini">
                 <el-option v-for="(item,index) in time_ipList" :key="'time'+index" :label="item.value" :value="item.id" />
               </el-select>
             </div>
             <div class="left_item">
-              主体名称：<el-input v-model="ipName" style="width:200px" />
+              主体名称：<el-input v-model="ipName" style="width:200px" size="mini" />
+              <span v-show="ipName===''" style="color:red">(必填)</span>
+            </div>
+            <div class="left_item">
+              <el-button size="mini" type="primary" @click="submit_addIP">确定</el-button>
             </div>
           </div>
-          <div class="right">
-            <div class="right_condition">
-              <div>
-                站点:
-                <storeChoose @changeMallList="changeMallList" />
-                <el-button @click="dialog_search_IPMall">查询</el-button>
-              </div>
-              <el-check v-model="showUserIP">显示已绑定ip店铺</el-check>
-              <div class="right_table">
-                <el-table
-
-                  ref="multipleTable"
-                  :row-key="generateUUID"
-                  :data="dialog_mallList"
-                >
-                  <el-table-column
-                    type="selection"
-                    width="55"
-                  />
-                  <el-table-column prop="" label="序号" />
-                  <el-table-column prop="" label="站点" />
-                  <el-table-column prop="" label="店铺名称" />
-                  <el-table-column prop="" label="已绑定公司主体名称" />
-                </el-table>
-              </div>
+          <!-- 新增自有IP公司主体 -->
+          <div v-if="Typeis==='ipPerson'">
+            <div class="left_item">
+              ip区域：
+              <el-select v-model="query_person.ipRegion" size="mini">
+                <el-option v-for="(item,index) in region_ipList" :key="'region'+index" :label="item.value" :value="item.id" />
+              </el-select>
+            </div>
+            <div class="left_item">
+              主体名称：<el-input v-model="query_person.ipName" style="width:200px" size="mini" />
+              <span v-show="query_person.ipName===''" style="color:red">(必填)</span>
+            </div>
+            <div class="left_item">
+              自有ip类型：
+              <el-radio-group v-model="query_person.ipType">
+                <el-radio :label="3">SSP</el-radio>
+                <el-radio :label="6">SS</el-radio>
+                <el-radio :label="9">HTTP</el-radio>
+                <el-radio :label="9">链接</el-radio>
+              </el-radio-group>
+            </div>
+            <div class="left_item">
+              服务器ip：<el-input v-model="query_person.ipService" style="width:200px" size="mini" />
+              <span v-show="query_person.ipService===''" style="color:red">(必填)</span>
+            </div>
+            <div class="left_item">
+              服务器端口：<el-input v-model="query_person.ipPort" style="width:200px" size="mini" />
+              <span v-show="query_person.ipPort===''" style="color:red">(必填)</span>
+            </div>
+            <div class="left_item">
+              密码：<el-input v-model="query_person.ipPsd" style="width:200px" size="mini" />
+            </div>
+            <div class="left_item">
+              加密方式：
+              <el-select v-model="query_person.ipPsdMethod" size="mini">
+                <el-option v-for="(item,index) in ipPsdMethodList" :key="'ipPsd'+index" :label="item.value" :value="item.id" />
+              </el-select>
+              <span v-show="query_person.ipPsdMethod===''" style="color:red">(必填)</span>
+            </div>
+            <div class="left_item">
+              协议参数：<el-input v-model="query_person.agreePro" style="width:200px" size="mini" />
+              <!-- <span v-show="query_person.agreePro===''" style="color:red">(非必填)</span> -->
+            </div>
+            <div class="left_item">
+              协议：<el-input v-model="query_person.agree" style="width:200px" size="mini" />
+              <span v-show="query_person.agree===''" style="color:red">(必填)</span>
+            </div>
+            <div class="left_item">
+              混淆：<el-input v-model="query_person.mix" style="width:200px" size="mini" />
+              <span v-show="query_person.mix===''" style="color:red">(必填)</span>
+            </div>
+            <div class="left_item">
+              混淆参数：<el-input v-model="query_person.mixPro" style="width:200px" size="mini" />
+              <span v-show="query_person.mixPro===''" style="color:red">(必填)</span>
+            </div>
+            <div class="left_item">
+              <el-button size="mini" type="primary" @click="submit_IpPersion">确定</el-button>
+            </div>
+          </div>
+        </div>
+        <div class="right">
+          <div class="right_condition">
+            <div>
+              <storeChoose @changeMallList="changeMallList" />
+              <el-button type="primary" size="mini" @click="dialog_search_IPMall">查询</el-button>
+            </div>
+            <el-checkbox v-model="showUserIP">显示已绑定ip店铺</el-checkbox>
+            <div class="right_table" style="border:1px solid #C0C4CC">
+              <el-table
+                ref="multipleTable"
+                height="400px"
+                :header-cell-style="{'background': '#f7fafa'}"
+                :row-key="generateUUID"
+                :data="dialog_mallList"
+              >
+                <el-table-column
+                  type="selection"
+                  width="55"
+                />
+                <el-table-column prop="" label="序号" />
+                <el-table-column prop="" label="站点" />
+                <el-table-column prop="" label="店铺名称" />
+                <el-table-column prop="" label="已绑定公司主体名称" />
+              </el-table>
             </div>
           </div>
         </div>
@@ -204,6 +269,26 @@ export default {
 
   data() {
     return {
+      Typeis: '',
+      dialogvisible: false,
+      ipPsdMethodList: [
+        { id: 1, value: 'none' },
+        { id: 2, value: 'sfsdf' },
+        { id: 3, value: 'werwr' }
+      ],
+      query_person: {
+        ipRegion: '',
+        ipName: '',
+        ipType: '',
+        ipService: '',
+        ipPort: '',
+        ipPsd: '',
+        ipPsdMethod: '',
+        agreePro: '',
+        agree: '',
+        mix: '',
+        mixPro: ''
+      },
       dialog_mallList: [],
       showUserIP: false,
       ipName: '', // 主体名称
@@ -221,7 +306,6 @@ export default {
         { id: 2, value: '泰国-曼谷' },
         { id: 3, value: '菲律宾-预售' }
       ],
-      dialog_addmaster: true,
       tableList: [],
       page: 1,
       pageSize: 20,
@@ -248,10 +332,35 @@ export default {
   },
   created() {
     this.initDate()
-    this.getTableList()// tableList
-    this.GetCloudIPAreaList()// 获取IP区域列表
+    // this.getTableList()// tableList
+    // this.GetCloudIPAreaList()// 获取IP区域列表
   },
   methods: {
+    // 续费一个月
+    async timeToMonth(period) {
+      // period=1 一个月 2一季度
+      const targetId = ''
+      const uid = ''
+      const uuid = ''
+      const data = await this.$YipService.RenewIP(targetId, uid, uuid, period)
+    },
+    // 获取ip价格
+    async GetIPPrice() {
+      const lineId = '' // 所选ip区域的id
+      const data = await this.$YipService.GetIPPrice(lineId)
+      console.log('dialog_ipPrice', data)
+    },
+    // 新增自有主体---提交
+    submit_IpPersion() {
+
+    },
+    // 新增公司主体---提交
+    submit_addIP() {
+      if (this.ipName === '') {
+        this.$message.warning('主体名称不能为空！')
+        return false
+      }
+    },
     // dialog 多选
     // 方法
     generateUUID() {
@@ -279,7 +388,7 @@ export default {
     // 获取IP区域列表
     async GetCloudIPAreaList() {
       const data = await this.$YipService.GetCloudIPAreaList()
-      console.log('region_ip', data)
+      console.log('获取ip列表', data)
     },
     // 获取店铺信息
     changeMallList(val) {
@@ -289,45 +398,27 @@ export default {
     // ip- tableList
     async getTableList() {
       const params = {}
-      console.log('111')
-      // const userInfo = await this.$appConfig.getUserInfo()
-      // console.log('55', userInfo)
-      // params.uid = userInfo.muid
+      const userInfo = await this.$appConfig.getUserInfo()
+      const mallIdStr = ''
+      params.uid = userInfo.muid
       params.uuid = ''
+      params.mall_ids = mallIdStr || ''
       params.ip_alias = ''
-      params.source = 1
+      params.source = this.queryParam.source
       params.status = ''
+      params.ip_address = this.queryParam.ip
       params.supplier_info = ''
       params.expiration_dates = ''
       params.ip_id = ''
-      // console.log(this.$YipService.GetIpList())
+      console.log('params', params)
       const res = await this.$YipService.GetIpList(JSON.stringify(params))
-      // console.log(res, '111')
+
       console.log(res)
     },
     // 分页递增
     indexMethod(index) {
       index = (index + 1) + (this.page - 1) * this.pageSize
       return index
-    },
-    // 多选
-    handleSelectionChange(val) {
-    // 获取参数
-    },
-    generateUUID() {
-      var d = new Date().getTime()
-      if (window.performance && typeof window.performance.now === 'function') {
-        d += performance.now() // use high-precision timer if available
-      }
-      var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(
-        /[xy]/g,
-        function(c) {
-          var r = (d + Math.random() * 16) % 16 | 0
-          d = Math.floor(d / 16)
-          return (c === 'x' ? r : (r & 0x3) | 0x8).toString(16)
-        }
-      )
-      return uuid
     },
     // 初始化时间
     initDate() {
@@ -388,6 +479,20 @@ export default {
         margin-right: 5px;
       }
     }
+    }
+    .dialog_addip{
+      .el-dialog{
+        .el-dialog__body{
+          display: flex;
+          align-items: center;
+          .left{
+            padding: 5px;
+            .left_item{
+              margin-bottom: 10px;
+            }
+          }
+        }
+      }
     }
 }
 </style>
