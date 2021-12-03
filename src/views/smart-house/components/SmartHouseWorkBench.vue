ab0ebb9c17309be243842c97770872ca8f09a64e
陈紫微 <chenziwei@91cyt.com>
2021-12-03 15:42:04 +0800

    仓库工作台导出

--- src/views/smart-house/components/SmartHouseWorkBench.vue
+++ src/views/smart-house/components/SmartHouseWorkBench.vue
@@ -141,7 +141,7 @@
         </div>
         <div class="select-item" style="margin-top:10px">
           <el-button type="primary" style="margin-left: 4px" size="mini" class="m-80" :loading="orderPackageLoading" @click="orderPackage">搜索</el-button>
-          <el-button v-if="!isExport" type="primary" size="mini" class="m-80" @click="tableToExcel">导出数据</el-button>
+          <el-button v-if="!isExport" :loading="exportLoading" style="width:100px" type="primary" size="mini" class="m-80" @click="tableToExcel(1)">导出数据</el-button>
           <el-progress v-else style="width: 160px; display: inline-block; margin-left: 10px" :text-inside="true" :stroke-width="26" :percentage="((exportNum * 100) / total).toFixed(2) - 0" />
           <el-button type="primary" size="mini" class="m-80" style="width:120px" @click="remarkFun">批量更新用户备注</el-button>
           <el-button type="primary" size="mini" @click="closelogData">清空日志</el-button>
@@ -171,7 +171,9 @@
             {{ scope.$index + 1 + (currentPage - 1) * pageSize }}
           </template>
         </el-table-column>
-        <el-table-column label="站点" min-width="80px" prop="country" fixed />
+        <el-table-column label="站点" min-width="80px" prop="country" fixed>
+          <template slot-scope="{row}"><span>{{ row.country | chineseSite }}</span></template>
+        </el-table-column>
         <el-table-column label="店铺名称" min-width="150px" prop="mall_alias_name">
           <!-- <template slot-scope="{row}"><span>{{ row.mall_alias_name ? row.mall_alias_name :row.platform_mall_name }}</span></template> -->
         </el-table-column>
@@ -528,7 +530,9 @@
 
         <div class="compareData_item">
           <el-table :header-cell-style="{ background: '#f7fafa' }" :data="compareDataList" :row-style="{ height: '50px' }" max-height="400">
-            <el-table-column prop="country" label="站点" min-width="80px" align="center" fixed />
+            <el-table-column prop="country" label="站点" min-width="80px" align="center" fixed>
+              <template slot-scope="{row}"><span>{{ row.country | chineseSite }}</span></template>
+            </el-table-column>
             <el-table-column prop="mall_alias_name" label="店铺名称" min-width="150px" align="center">
               <template slot-scope="{row}"><span>{{ row.mall_alias_name ? row.mall_alias_name :row.platform_mall_name }}</span></template>
             </el-table-column>
@@ -683,6 +687,7 @@ export default {
   },
   data() {
     return {
+      exportLoading: false,
       detailLoading: false, // 查看订单包裹详情加载
       showlog: true,
       // colorStyle: 'width:50px;height:20px;background-color:',
@@ -1273,12 +1278,12 @@ export default {
             }
             item.mall_alias_name = getValue(this.shopAccountList, 'label', 'id', item.sys_mall_id)
             // item.site = getValue(this.siteList, 'label', 'value', item.country)
-            item.statusText = getValue(statusList, 'label', 'deliveryStatus', item.delivery_status)
+            // item.statusText = getValue(statusList, 'label', 'deliveryStatus', item.delivery_status)
             item.exceptionText = getValue(exceptionList, 'label', 'exception_type', item.exception_type)
             item.orderStatusText = getValue(orderStatusList, 'label', 'order_status', item.order_status)
             // item.colorText = getValue(this.colorLogoList, 'Colorinfo', 'id', item.color_id) || '无'
             item.colorinfo = getColorinfo(this.colorLogoList, item.color_id) || ''
-            item.country = this.shopeeConfig.getSiteCode(item.country)
+            // item.country = this.shopeeConfig.getSiteCode(item.country)
             this.tableData.push(item)
           }
           // console.log('colorLogoList', this.colorLogoList)
@@ -1698,130 +1703,162 @@ export default {
       }
     },
     // 导出
-    tableToExcel() {
-      // 要导出的json数据
-      if (this.multipleSelection.length > 0) {
-        this.importBilling([this.multipleSelection])
-      } else {
-        if (!this.total) {
-          // return this.$notify({
-          //   title: '订单信息',
-          //   type: 'warning',
-          //   message: `没有可以导出的订单`
-          // })
-          this.$message.warning('没有可以导出的订单')
-        }
-        this.exportOrderList = []
-        // this.dialogVisible1 = true
-      }
-    },
-    // 实时导出
-    async exportOrders(page, isRef) {
-      this.isExport = this.isExport ? this.isExport : new Date() - 0
-      const params = {
+    async tableToExcel(page) {
+      this.exportLoading = true
+      const sysMallIds = this.shopAccount.toString()
+      const query = {
+        page: page,
+        pageSize: 200,
         status: '',
         packageOrderSn: this.packageOrderSn,
-        statuoriginalTrackingNumbers: this.statuoriginalTrackingNumbers,
+        originalTrackingNumber: this.statuoriginalTrackingNumbers,
         exceptionType: this.exceptionType,
         deliveryStatus: this.deliveryStatus,
         colorLabelId: this.colorLabelId,
-        // sysMallIds: this.shopAccount.join(',')
-        sysMallIds: this.shopAccount.toString()
+        sysMallIds
       }
-      params.page = page
-      params.pageSize = 200
-      params.isExport = this.isExport
       if (this.createdTime && this.createdTime.length) {
-        params.createdTime = this.setDateFmt(this.createdTime).join('/')
+        query.createdTime = this.setDateFmt(this.createdTime).join('/')
       }
       if (this.storageTime && this.storageTime.length) {
-        params.storageTime = this.setDateFmt(this.storageTime).join('/')
+        query.storageTime = this.setDateFmt(this.storageTime).join('/')
       }
       if (this.outboundTime && this.outboundTime.length) {
-        params.outboundTime = this.setDateFmt(this.outboundTime).join('/')
+        query.outboundTime = this.setDateFmt(this.outboundTime).join('/')
       }
-      const { data } = await this.$api.orderPackage(params)
-      // console.log(data, 2222)
-      if (data && data.data && data.data.data) {
-        const newData = data.data.data
-        for (let i = 0; i < newData.length; i++) {
-          const item = newData[i]
-          if (this.site !== '' && item.country !== this.site) {
-            continue
+      try {
+        const res = await this.$api.orderPackage(query)
+        const data = res.data
+        if (data.code === 200) {
+          const list = data.data.data
+          for (let i = 0; i < list.length; i++) {
+            const item = list[i]
+            if (this.site !== '' && item.country !== this.site) {
+              continue
+            }
+            item.mall_alias_name = getValue(this.shopAccountList, 'label', 'id', item.sys_mall_id)
+            item.exceptionText = getValue(exceptionList, 'label', 'exception_type', item.exception_type)
+            item.colorinfo = getColorinfo(this.colorLogoList, item.color_id) || ''
+            this.exportOrderList.push(item)
           }
-          item.mall_alias_name = getValue(this.shopAccountList, 'label', 'id', item.sys_mall_id)
-          // item.site = getValue(this.siteList, 'label', 'value', item.country)
-          item.statusText = getValue(statusList, 'label', 'deliveryStatus', item.delivery_status)
-          item.exceptionText = getValue(exceptionList, 'label', 'exception_type', item.exception_type)
-          item.orderStatusText = getValue(orderStatusList, 'label', 'order_status', item.order_status)
-          // item.colorText = getValue(this.colorLogoList, 'label', 'id', item.color_id) || '无'
-          item.colorinfo = getColorinfo(this.colorLogoList, item.color_id) || ''
-        }
-        this.total = data.data.total
-        if (this.exportOrderList[this.exportIndex]) {
-          this.exportOrderList[this.exportIndex].push(...newData)
         } else {
-          this.exportOrderList[this.exportIndex] = newData
-        }
-        if (this.exportOrderList[this.exportIndex].length >= 10000) {
-          this.exportOrderList[this.exportIndex + 1] = []
-          this.exportIndex++
-        }
-        this.exportNum = this.exportIndex * 10000 + this.exportOrderList[this.exportIndex].length
-        if (this.exportNum >= this.total) {
-          this.isExport = false
-          this.importBilling(this.exportOrderList)
-          //  loading.close()
-        } else {
-          this.exportOrders(params.page + 1)
-        }
-      } else {
-        if (isRef) {
-          this.isExport = false
-
-          // this.$notify({
-          //   title: '仓库管理',
-          //   type: 'error',
-          //   message: data.message
-          // })
           this.$message.error(data.message)
-        } else {
-          this.exportOrders(params.page, true)
         }
+      } catch (err) {
+        this.$message.error(err)
+      }
+
+      if (this.exportOrderList.length < this.total) {
+        this.tableToExcel(page + 1)
+      } else {
+        this.exportLoading = false
+        this.importBilling(this.exportOrderList)
       }
     },
+    // 实时导出
+    // async exportOrders(page, isRef) {
+    //   this.isExport = this.isExport ? this.isExport : new Date() - 0
+    //   const params = {
+    //     status: '',
+    //     packageOrderSn: this.packageOrderSn,
+    //     statuoriginalTrackingNumbers: this.statuoriginalTrackingNumbers,
+    //     exceptionType: this.exceptionType,
+    //     deliveryStatus: this.deliveryStatus,
+    //     colorLabelId: this.colorLabelId,
+    //     // sysMallIds: this.shopAccount.join(',')
+    //     sysMallIds: this.shopAccount.toString()
+    //   }
+    //   params.page = page
+    //   params.pageSize = 200
+    //   params.isExport = this.isExport
+    //   if (this.createdTime && this.createdTime.length) {
+    //     params.createdTime = this.setDateFmt(this.createdTime).join('/')
+    //   }
+    //   if (this.storageTime && this.storageTime.length) {
+    //     params.storageTime = this.setDateFmt(this.storageTime).join('/')
+    //   }
+    //   if (this.outboundTime && this.outboundTime.length) {
+    //     params.outboundTime = this.setDateFmt(this.outboundTime).join('/')
+    //   }
+    //   const { data } = await this.$api.orderPackage(params)
+    //   // console.log(data, 2222)
+    //   if (data && data.data && data.data.data) {
+    //     const newData = data.data.data
+    //     for (let i = 0; i < newData.length; i++) {
+    //       const item = newData[i]
+    //       if (this.site !== '' && item.country !== this.site) {
+    //         continue
+    //       }
+    //       item.mall_alias_name = getValue(this.shopAccountList, 'label', 'id', item.sys_mall_id)
+    //       // item.site = getValue(this.siteList, 'label', 'value', item.country)
+    //       // item.statusText = getValue(statusList, 'label', 'deliveryStatus', item.delivery_status)
+    //       item.exceptionText = getValue(exceptionList, 'label', 'exception_type', item.exception_type)
+    //       item.orderStatusText = getValue(orderStatusList, 'label', 'order_status', item.order_status)
+    //       // item.colorText = getValue(this.colorLogoList, 'label', 'id', item.color_id) || '无'
+    //       item.colorinfo = getColorinfo(this.colorLogoList, item.color_id) || ''
+    //     }
+    //     this.total = data.data.total
+    //     if (this.exportOrderList[this.exportIndex]) {
+    //       this.exportOrderList[this.exportIndex].push(...newData)
+    //     } else {
+    //       this.exportOrderList[this.exportIndex] = newData
+    //     }
+    //     if (this.exportOrderList[this.exportIndex].length >= 10000) {
+    //       this.exportOrderList[this.exportIndex + 1] = []
+    //       this.exportIndex++
+    //     }
+    //     this.exportNum = this.exportIndex * 10000 + this.exportOrderList[this.exportIndex].length
+    //     if (this.exportNum >= this.total) {
+    //       this.isExport = false
+    //       this.importBilling(this.exportOrderList)
+    //       //  loading.close()
+    //     } else {
+    //       this.exportOrders(params.page + 1)
+    //     }
+    //   } else {
+    //     if (isRef) {
+    //       this.isExport = false
+
+    //       // this.$notify({
+    //       //   title: '仓库管理',
+    //       //   type: 'error',
+    //       //   message: data.message
+    //       // })
+    //       this.$message.error(data.message)
+    //     } else {
+    //       this.exportOrders(params.page, true)
+    //     }
+    //   }
+    // },
     // 生成导出文件--订单
-    importBilling(exportOrderList) {
+    importBilling() {
       // console.log(exportOrderList, this.exportIndex)
       let num = 1
-      let str = `<tr><td>编号</td><td>站点</td><td>店铺名称</td><td>仓库</td><td>颜色标识</td><td>订单编号</td><td>数量</td><td>包裹重量(g)</td><td>发货金额</td><td>运输方式</td><td>货物类型</td><td>是否等待子包裹发货</td>
+      let str = `<tr><td>编号</td><td>站点</td><td>店铺名称</td><td>仓库</td><td>颜色标识</td><td>订单编号</td><td>数量</td><td>包裹重量(g)</td><td>发货金额</td><td>运输方式</td><td>货物类型</td><td>不等待子包裹发货</td>
             <td>订单发货状态</td><td>异常类型</td><td>订单创建时间</td><td>订单平台状态</td><td>截止发货时间</td><td>入库时间</td><td>出库时间</td>
             <td>入库图片</td><td>出库图片</td><td>仓库备注</td><td>用户备注</td>
             <td>增值服务名称</td><td>增值服务金额</td><td>增值服务备注</td>
             </tr>`
-      for (let index = 0; index <= this.exportIndex; index++) {
-        const jsonData = exportOrderList[index]
-        console.log(jsonData)
-        jsonData.forEach((item) => {
-          str += `<tr><td>${num++}</td>
-                <td>${this.$filters.chineseSite(item.country) + '\t'}</td>
+      this.exportOrderList.forEach(item => {
+        str += `<tr><td>${num++}</td>
+                <td>${item.country ? this.$filters.chineseSite(item.country) : '' + '\t'}</td>
                 <td>${item.mall_alias_name ? item.mall_alias_name : '' + '\t'}</td>
                 <td>${item.warehouse_name}</td>
                 <td>${item.colorinfo ? item.colorinfo.name : '' + '\t'}</td>
-                <td style="mso-number-format:'\@';">${item.package_order_sn ? item.package_order_sn : ''}</td>
-                <td>${item.goodsCount ? item.goodsCount : ''}</td>
-                <td>${item.package_weight ? item.package_weight : ''}</td>
-                <td>${item.amount ? item.amount + '元' : ''}</td>
-                <td>${item.transport_type ? this.transportType[item.transport_type] : ''}</td>
-                <td>${item.package_type ? this.packageType[item.package_type] : ''}</td>
-                <td>${item.is_mark_outbound > 0 ? '是' : '否'}</td>
-                <td>${item.statusText ? item.statusText : ''}</td>
-                <td>${item.exceptionText ? item.exceptionText : ''}</td>
-                <td>${item.order_created_time}</td>
-                <td>${item.order_status ? this.orderStatusList[item.order_status] : ''}</td>
-                <td>${item.order_ship_by_date ? item.order_ship_by_date : ''}</td>
-                <td>${item.storage_time ? item.storage_time : ''}</td>
-                <td>${item.outbound_time ? item.outbound_time : ''}</td>
+                <td style="mso-number-format:'\@';">${item.package_order_sn ? item.package_order_sn : '' + '\t'}</td>
+                <td>${item.goodsCount ? item.goodsCount : '' + '\t'}</td>
+                <td>${item.package_weight ? item.package_weight : '' + '\t'}</td>
+                <td>${item.amount ? item.amount + '元' : '' + '\t'}</td>
+                <td>${item.transport_type ? this.transportType[item.transport_type] : '' + '\t'}</td>
+                <td>${item.package_type ? this.packageType[item.package_type] : '' + '\t'}</td>
+                <td>${item.is_mark_outbound > 0 ? '是' : '否' + '\t'}</td>
+                <td>${item.delivery_status ? this.delivery_statusList[item.delivery_status] : '' + '\t'}</td>
+                <td>${item.exceptionText ? item.exceptionText : '' + '\t'}</td>
+                <td>${item.order_created_time + '\t'}</td>
+                <td>${item.order_status ? this.orderStatusList[item.order_status] : '' + '\t'}</td>
+                <td>${item.order_ship_by_date ? item.order_ship_by_date : '' + '\t'}</td>
+                <td>${item.storage_time ? item.storage_time : '' + '\t'}</td>
+                <td>${item.outbound_time ? item.outbound_time : '' + '\t'}</td>
                 <td>${item.storage_image ? item.storage_image : '' + '\t'}</td>
                  <td>${item.outbound_image ? item.outbound_image : '' + '\t'}</td>
                <td>${item.remark ? item.remark : '' + '\t'}</td>
@@ -1829,10 +1866,8 @@ export default {
                <td>${item.ext_service ? item.ext_service.name : '' + '\t'}</td>
                <td>${item.ext_service ? item.ext_service.price : '' + '\t'}</td>
                <td>${item.ext_service ? item.ext_service.remark : '' + '\t'}</td>
-
                 </tr>`
-        })
-      }
+      })
       // Worksheet名
       const worksheet = `仓库包裹数据${new Date(Date.now() + 8 * 3600 * 1000).toISOString().slice(0, 10)}`
       // const uri = 'data:application/vnd.ms-excel;base64,'
