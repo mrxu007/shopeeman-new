256bd12bb1979f04ed7ff095b3d128691769cdf1
谢君蕊 <xiejunrui@91cyt.com>
2022-01-19 20:28:37 +0800

    更新代码

--- src/module-api/goods-manager-api/goods-list.js
+++ src/module-api/goods-manager-api/goods-list.js
@@ -97,12 +97,12 @@ export default class GoodsList {
     params['mallId'] = val.platform_mall_id
     try {
       const res = await this._this.$shopeemanService.getChinese(val.country, '/api/marketing/v3/add_on_deal/aggr_sub_item_list/?', params)
-      const jsonData = this.isJsonString(res)
-      if (jsonData.status === 200) {
-        const data = this.isJsonString(jsonData.data)
-        return { code: 200, data: data.data }
+      const jsonData = this.isJsonString(this.isJsonString(res).data)
+      console.log('获取子商品加购活动', jsonData)
+      if (jsonData.message === 'success') {
+        return { code: 200, data: jsonData.data }
       }
-      return { code: 201, data: jsonData.statusText }
+      return { code: 201, data: jsonData.message }
     } catch (error) {
       return { code: -2, data: `子商品加购活动列表异常： ${error}` }
     }
