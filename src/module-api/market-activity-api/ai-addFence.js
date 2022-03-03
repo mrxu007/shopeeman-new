0ac896a0ba98247c150d559290a3388693fe0b7b
陈紫微 <chenziwei@91cyt.com>
2022-03-03 09:41:27 +0800

    关注店铺搜索

--- src/module-api/market-activity-api/ai-addFence.js
+++ src/module-api/market-activity-api/ai-addFence.js
@@ -1,31 +1,179 @@
+import GUID from '@/util/guid'
 export default class GoodsDiscount {
   constructor(that) {
     this._this = that
   }
+  // str转码
+  urlencode(str) {
+    str = (str + '').toString()
+
+    return encodeURIComponent(str).replace(/!/g, '%21').replace(/'/g, '%27').replace(/\(/g, '%28')
+      .replace(/\)/g, '%29').replace(/\*/g, '%2A').replace(/%20/g, '+')
+  }
+  guid() {
+    return 'xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx'.replace(
+      /[xy]/g,
+      function(c) {
+        var r = (Math.random() * 16) | 0
+        var v = c == 'x' ? r : (r & 0x3) | 0x8
+        return v.toString(16)
+      }
+    )
+  }
   // 搜索关注店铺
   async addCancerMall(val) {
-    const { item } = val
+    const item = val
     try {
+      const keywordEncode = encodeURI(item.keyword)
       const params = {}
-      params['country'] = item.country
       params['mallId'] = item.mallId
+
       params['by'] = item.by
       params['keyword'] = item.keyword
       params['limit'] = item.limit
       params['newest'] = item.newest
       params['order'] = item.order
       params['page_type'] = item.page_type
+      params['scenario'] = 'PAGE_GLOBAL_SEARCH'
+      // params['rating_filter'] = item.rating_filter
+      // params['skip_autocorrect'] = 1
       params['version'] = 2
-
-      const res = await this._this.$shopeemanService.getChineseBuyer(item.country, '/api/v2/search_items/?', params, {
+      const strGuid = this.guid()
+      console.log('36', keywordEncode)
+      const res = await this._this.$shopeemanService.getChineseBuyer(item.country, '/api/v4/search/search_items?', params, {
         headers: {
-          referer: `/search?keyword=${item.keyword}}`,
-          Accept: 'application/json, application/xml, text/json, text/x-json, text/javascript, text/xml'
+          'Content-type': 'application/json',
+          referer: `/search?keyword=${keywordEncode}&trackingId=searchhint-1646186021-961459e5-99cb-11ec-85af-8a3d66826fb4`,
+          'Accept': 'application/json',
+          'If-None-Match-': this.guid(),
+          'x-api-source': 'pc',
+          'sec-fetch-site': 'same-origin',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-dest': 'empty',
+          'X-CSRFToken': strGuid
+
         }
       })
-      return res
+      return JSON.parse(res)
     } catch (error) {
       return { code: -2, data: `店铺搜索异常 ${error}` }
+    }
+  }
+  // 获取粉丝数
+  async getFllower(val) {
+    const item = val
+    try {
+      const params = {}
+      params['mallId'] = item.mallId
+
+      params['offset'] = item.offset
+      params['limit'] = item.limit
+      params['offset_of_offset'] = item.offset_of_offset
+      params['_'] = item.timeStamp
+      const res = await this._this.$shopeemanService.getChineseBuyer(item.country, `/shop/${item.mallId}/followers/?`, params, {
+        headers: {
+          'Content-type': 'text/html; charset=utf-8',
+          referer: `/shop/${item.mallId}/followers/?__classic__=1`,
+          'accept': '*/*',
+          'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Mobile Safari/537.36',
+          'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="98", "Google Chrome";v="98"',
+          'x-requested-with': 'XMLHttpRequest',
+          'If-None-Match-': this.guid(),
+          'sec-ch-ua-mobile': '?1',
+          'sec-ch-ua-platform': '"Android"',
+          'sec-fetch-site': 'same-origin',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-dest': 'empty',
+          'Accept-Encoding': 'gzip, deflate, br'
+
+        }
+      })
+      // 正则表达转换
+      const data = JSON.parse(res).data
+      const reg = /<li\s*?data-follower-shop-id=.(?<ShopId>[0-9]+?).\s*?data-follower[^>]+>\s*?<a(([^>]+username='(?<UserName>[^>]+)'\s*?userid=.(?<UserId>[0-9]+?).\s*?class=.shop-href.)|())>/igs
+      let info = {}
+      const mallList = []
+      do {
+        info = reg.exec(data)
+        console.log(info.groups)
+        mallList.push(info.groups)
+      } while (info = reg.exec(data))
+
+      if (res) {
+        return { code: 200, data: mallList }
+      } else {
+        return { code: 201, data: [], message: '请求失败' }
+      }
+    } catch (error) {
+      return { code: -2, data: `粉丝请求异常 ${error}` }
+    }
+  }
+  // 关注用户
+  async buyerFollow(val) {
+    const item = val
+    try {
+      const params = {}
+      params['mallId'] = item.mallId
+      params['csrfmiddlewaretoken'] = this.guid().replaceAll('-', '')
+      const res = await this._this.$shopeemanService.postChineseBuyer(item.country, `/shop/${item.UserId}/followers/?`, params, {
+        headers: {
+          'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+          referer: `/shop/${item.mallId}/followers/?__classic__=1`,
+          'accept': '*/*',
+          'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Mobile Safari/537.36',
+          'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="98", "Google Chrome";v="98"',
+          'x-requested-with': 'XMLHttpRequest',
+          'If-None-Match-': this.guid(),
+          'sec-ch-ua-mobile': '?1',
+          'sec-ch-ua-platform': '"Android"',
+          'sec-fetch-site': 'same-origin',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-dest': 'empty',
+          'Accept-Encoding': 'gzip, deflate, br'
+
+        }
+      })
+      if (res) {
+        return { code: 200, data: true }
+      } else {
+        return { code: 201, data: false, message: '请求失败' }
+      }
+    } catch (error) {
+      return { code: -2, data: `关注请求异常 ${error}` }
+    }
+  }
+  // 取消关注用户
+  async buyerUNFollow(val) {
+    const item = val
+    try {
+      const params = {}
+      params['mallId'] = item.mallId
+      params['csrfmiddlewaretoken'] = this.guid().replaceAll('-', '')
+      const res = await this._this.$shopeemanService.postChineseBuyer(item.country, `/shop/${item.UserId}/unfollowers/?`, params, {
+        headers: {
+          'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+          referer: `/shop/${item.mallId}/followers/?__classic__=1`,
+          'accept': '*/*',
+          'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Mobile Safari/537.36',
+          'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="98", "Google Chrome";v="98"',
+          'x-requested-with': 'XMLHttpRequest',
+          'If-None-Match-': this.guid(),
+          'sec-ch-ua-mobile': '?1',
+          'sec-ch-ua-platform': '"Android"',
+          'sec-fetch-site': 'same-origin',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-dest': 'empty',
+          'Accept-Encoding': 'gzip, deflate, br'
+
+        }
+      })
+      if (res) {
+        return { code: 200, data: true }
+      } else {
+        return { code: 201, data: false, message: '请求失败' }
+      }
+    } catch (error) {
+      return { code: -2, data: `关注请求异常 ${error}` }
     }
   }
 }
--- src/views/market-activity/components/MarketActivityExplosivePowderArtifact.vue
+++ src/views/market-activity/components/MarketActivityExplosivePowderArtifact.vue
@@ -6,6 +6,7 @@
         <storeChoose style="margin-left:-2px" @changeMallList="changeMallList" />
         <el-button type="primary" size="mini" @click="clearLog">清除日志</el-button>
         <el-button type="primary" size="mini" @click="setQurey">参数设置</el-button>
+        <el-button @click="test">aa</el-button>
         <el-checkbox v-model="showlog" style="margin-top:5px;margin-left:5px">隐藏日志</el-checkbox>
       </li>
       <!-- row2 -->
@@ -113,15 +114,27 @@
             @selection-change="handleSelectionChange"
           >
             <el-table-column align="center" width="55" type="selection" />
-            <el-table-column prop="" label="店铺ID" min-width="100px" align="center" />
-            <el-table-column prop="" label="产品标题" min-width="150px" align="center" />
-            <el-table-column prop="" label="主图" min-width="100px" align="center" fixed="right" />
-            <el-table-column prop="" label="点赞数" min-width="100px" align="center" fixed="right" />
-            <el-table-column prop="" label="评论数" min-width="100px" align="center" fixed="right" />
-            <el-table-column prop="" label="产品评分" min-width="100px" align="center" fixed="right" />
-            <el-table-column prop="" label="操作" min-width="100px" align="center" fixed="right">
-              <template>
-                <el-button type="primary" size="mini">查看产品</el-button>
+            <el-table-column align="center" label="序号" width="55" type="index" />
+            <el-table-column prop="itemid" label="店铺ID" min-width="100px" align="center" />
+            <el-table-column prop="name" label="产品标题" min-width="150px" align="center" show-overflow-tooltip />
+            <el-table-column prop="" label="主图" min-width="100px" align="center" fixed="right">
+              <template v-slot="{row}">
+                <el-tooltip effect="light" placement="right-end" :visible-arrow="false" :enterable="false" style="width: 50px; height: 50px; display: inline-block">
+                  <div slot="content">
+                    <el-image :src="[row.image] | imageRender" style="width: 400px; height: 400px" />
+                  </div>
+                  <el-image :src="[row.image, true] | imageRender" style="width: 50px; height: 50px" />
+                </el-tooltip>
+              </template>
+            </el-table-column>
+            <el-table-column prop="liked_count" label="点赞数" min-width="100px" align="center" fixed="right" />
+            <el-table-column prop="totalRate" label="评论数" min-width="100px" align="center" fixed="right" />
+            <el-table-column prop="rating_star" label="产品评分" min-width="100px" align="center" fixed="right">
+              <template v-slot="{row}">{{ row.item_rating.rating_star.toFixed(2) }}</template>
+            </el-table-column>
+            <el-table-column prop="" label="操作" min-width="200px" align="center" fixed="right">
+              <template v-slot="{row}">
+                <el-button type="primary" size="mini" @click="open(row)">查看产品</el-button>
                 <el-button type="primary" size="mini">添加店铺</el-button>
               </template>
             </el-table-column>
@@ -149,6 +162,9 @@
 import StoreChoose from '../../../components/store-choose'
 import autoFollow from '@/views/user-config/autoFollow.vue'
 import MallManagerAPI from '@/module-api/market-activity-api/ai-addFence'
+import { batchOperation } from '@/util/util'
+import { data } from 'cheerio/lib/api/attributes'
+
 export default {
   name: 'BusinessAnalyseGoodsDiscount',
   components: {
@@ -170,14 +186,14 @@ export default {
       tableList: [], // 表格
       // 弹窗-店铺搜索
       dialog_mallSearch: false, // 弹窗
-      keyword: '', // 关键词
+      keyword: '手机', // 关键词
       minComment: '50', // 评论数
       maxComment: '5000',
       minLike: '100', // 点赞数
       maxLike: '10000',
       minScore: '4.5', // 评分数
       maxScore: '5',
-      mallNum: '10', // 店铺数
+      mallNum: '1', // 店铺数
       mallSearchList: [], // 弹窗列表
       sel_mallSearchList: [] // 多选店铺
     }
@@ -186,6 +202,26 @@ export default {
     this.getUserinfo() // 获取用户设置数据
   },
   methods: {
+    // 跳转链接
+    async open(val) {
+      const country = this.tableList[0].country
+      const data = {
+        mallId: val.shopid.toString()
+      }
+      const url = await this.$shopeemanService.getWebUrlLocal(country, data)
+      this.$BaseUtilService.openUrl(`${url}/product/${val.shopid}/${val.itemid}`)
+    },
+    async test() {
+      const params = {}
+      params['country'] = 'MY'
+      params['mallId'] = '268048262'
+
+      params['offset'] = 20
+      params['limit'] = 20
+      params['offset_of_offset'] = 0
+      params['timeStamp'] = new Date().getTime()
+      const res = await this.MallAPIInstance.getFllower(params)
+    },
     // 添加关注店铺--弹窗
     addCancerMall() {
       if (!this.tableList.length) {
@@ -194,44 +230,155 @@ export default {
       }
       this.dialog_mallSearch = true
     },
-    // 添加关注店铺--获取关注店铺ID
-    async searchMall(item) {
+    // 添加关注店铺--搜素店铺接口
+    async searchMallInstance(item) {
+      const params = {}
+      params['country'] = item.country
+      params['mallId'] = item.mallId
+
+      params['by'] = 'relevancy'
+      params['keyword'] = this.keyword
+      params['limit'] = 60
+      params['newest'] = item.newest
+      params['order'] = 'desc'
+      params['page_type'] = 'search'
+      params['rating_filter'] = this.userInfo.auto_attention_set.MinOrderEvaluation // 评分
+      try {
+        const res = await this.MallAPIInstance.addCancerMall(params)
+        if (res.status === 200) {
+          return JSON.parse(res.data).items
+        } else {
+          this.$refs.Logs.writeLog(`店铺搜索请求失败,230-line:${res.status}`, false)
+          return []
+        }
+      } catch (error) {
+        this.$refs.Logs.writeLog(`店铺搜索请求失败,234-line:${error}`, false)
+        return []
+      }
+    },
+    // 添加关注店铺--店铺条件过滤
+    filterList(goodsMalllist) {
+      let getList = []
+      const map = new Map()
+      goodsMalllist.forEach(el => { // 去重
+        if (!map.has(el.item_basic.itemid) && !map.has(el.item_basic.shopid)) {
+          map.set(el.itemid, el)
+        }
+      })
+      getList = [...map.values()]
+      console.log('266', 'liked_count,rating_star,totalRate')
+      const getList2 = getList.filter(el => { // 条件过滤
+        let flag = null
+        const totalRate = el.item_basic.item_rating.rating_count.reduce(function(prev, curr, idx, arr) {
+          return prev + curr
+        }) // 评论总数求和
+        if (Number(el.item_basic.liked_count) >= Number(this.minLike) && Number(el.item_basic.liked_count) <= Number(this.maxLike) && // 点赞
+             (el.item_basic.item_rating.rating_star).toFixed(2) >= Number(this.minScore) && el.item_basic.item_rating.rating_star.toFixed(2) <= Number(this.maxScore) && // 评分
+            totalRate >= Number(this.minComment) && totalRate <= Number(this.maxComment) // 评论
+        ) {
+          flag = true
+        } else {
+          flag = false
+        }
+        if (flag) {
+          return el
+        }
+      })
+      return getList2
+    },
+    // 添加关注店铺--搜素店铺
+    async searchMall(mall) {
       console.log(this.userInfo)
       if (!this.keyword) {
         this.$message.warning('请输入关键字')
         return
       }
-      const params = {}
-      params['country'] = item.country
-      params['mallId'] = item.mallId
-      params['by'] = 'relevancy'
-      params['keyword'] = this.keyword
-      params['limit'] = 50
-      params['newest'] = 0
-      params['order'] = 'desc'
-      params['page_type'] = 'search'
-      const res = await this.MallManagerAPI.addCancerMall(params)
-      let array = res.data.data || []
-      // 控制店铺商品上限
-      while (array.length < Number(this.userInfo.auto_attention_set.ProductMax)) {
-        params['newest'] += 50
-        const res = await this.MallManagerAPI.addCancerMall(params)
-        array = array.concat(array)
+      this.showlog = false
+      this.$refs.Logs.writeLog(`正在获取店铺信息......`, true)
+      const maxPage = Number(this.mallNum) // 搜索店铺数
+      if (maxPage >= 0) {
+        let AllgoodsMalllist = []
+        // 查询店铺
+        for (let i = 0; i < maxPage; i++) {
+          const params = {}
+          params['country'] = this.tableList[0].country
+          params['mallId'] = this.tableList[0].platform_mall_id // [country mallid 不影响搜索结果，去表格第一个]
+          params['newest'] = 60 * i
+          const goodsMalllist = await this.searchMallInstance(params)
+          if (goodsMalllist.length) {
+            // 店铺条件过滤
+            const fansMallsList = this.filterList(goodsMalllist)
+            console.log('299', fansMallsList)
+            if (fansMallsList.length) {
+              AllgoodsMalllist.push(...fansMallsList)
+              // 根据配置信息截取所需
+              if (AllgoodsMalllist.length >= maxPage) {
+                AllgoodsMalllist = AllgoodsMalllist.splice(0, maxPage) // 截取所需店铺
+                break
+              }
+            } else {
+              this.$refs.Logs.writeLog(`此关键词暂未搜索到数据，请更换关键词或重试！`)
+              break
+            }
+          } else {
+            this.$refs.Logs.writeLog(`数据已被过滤空，请更换关键词或放宽搜索条件后重试！`)
+            break
+          }
+        }
+        this.$refs.Logs.writeLog(`店铺搜索获取结束`)
+        this.mallSearchList = []
+        AllgoodsMalllist.forEach(el => {
+          el.item_basic.totalRate = el.item_basic.item_rating.rating_count.reduce(function(prev, curr, idx, arr) {
+            return prev + curr
+          })
+          this.mallSearchList.push(el.item_basic)
+        })
       }
-      this.mallSearchList = res.data.data
     },
     // 添加关注店铺--目标店铺--赋值给mallIDinfo
     addTargetMall(val) {
       const mallIDList = []
       this.sel_mallSearchList.forEach(el => {
-        mallIDList.push(el.mallID)
+        mallIDList.push(el.shopid)
       })
-      const str = mallIDList.toString().replace(',', '\n')
-      this.mallIDinfo = str
+      // const str = mallIDList.toString().replaceAll(',', '\n')
+      this.mallIDinfo = mallIDList.toString()
+      this.dialog_mallSearch = false
     },
     // 开始关注
-    startCancer() {
+    async startCancer() {
+      this.$refs.Logs.writeLog(`开始关注店铺......`, true)
+      await batchOperation(this.tableList, this.RunAttentionUsers)
+      this.$refs.Logs.writeLog(`店铺关注结束`, true)
+    },
+    // 开始关注-根据userinfo配置关注用户
+    RunAttentionUsers(item, count = { count: 1 }) {
+      try {
+        let shopDatas = []
+        if (this.userInfo.auto_attention_set.FollowType === 0) { // 关注店铺粉丝
+          item['offset'] = 0
+          shopDatas = this.GetShopLikes(item) // 获取粉丝信息
+        } else {
+          shopDatas = this.GetShopComment(item) // 店铺评价用户
+        }
+      } catch (e) {
 
+      } finally {
+        --count.count
+      }
+    },
+
+    // 获取粉丝信息
+    async GetShopLikes(item) {
+      const params = {}
+      params['country'] = item.country
+      params['mallId'] = item.platform_mall_id
+
+      params['offset'] = item.offset
+      params['limit'] = 20
+      params['offset_of_offset'] = 0
+      params['timeStamp'] = new Date().getTime()
+      const res = await this.MallAPIInstance.getFllower(params)
     },
     // 获取用户数据
     async getUserinfo() {
