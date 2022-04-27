45aeb0229f50f239288c05b38bd1ea781d60f68b
xiazuowei <xiazuowei@91cyt.com>
2022-04-27 21:11:07 +0800

    上线

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,8 @@
 const proPlugins = []
-// //如果当前是生产环境，则使用去掉console的插件
-// if (process.env.NODE_ENV === 'production') {
-//   proPlugins.push('transform-remove-console')
-// }
+// 如果当前是生产环境，则使用去掉console的插件
+if (process.env.NODE_ENV === 'production') {
+  proPlugins.push('transform-remove-console')
+}
 module.exports = {
   presets: ['@vue/cli-plugin-babel/preset'],
   plugins: [
