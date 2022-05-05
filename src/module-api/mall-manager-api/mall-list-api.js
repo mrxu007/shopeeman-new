4b804f7ef49867e708810ec09e4732b965df36b8
xiazuowei <xiazuowei@91cyt.com>
2022-05-05 21:58:14 +0800

    1

--- src/module-api/mall-manager-api/mall-list-api.js
+++ src/module-api/mall-manager-api/mall-list-api.js
@@ -484,7 +484,7 @@ export default class MallListAPI {
         if (res.data.code === 200) {
           let data = res?.data?.data
           total = data?.total || total
-          let mallList = data?.malls?.data || []
+          let mallList = data?.data || []
           tempMallList = [...tempMallList, ...mallList]
         }else{
           break
